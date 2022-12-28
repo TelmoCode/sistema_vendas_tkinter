d9c4b631b71c2c100cd692335a05dc2f91c92cc6
TelmoCode <telmogferreira@gmail.com>
2022-12-28 16:42:26 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import time
+import datetime
+import pygame,sys, random
+
+pygame.init()
+
+root = Tk()
+root.title("Sistema de Vendas")
+root.geometry('1352x720+0+0')
+
+root.configure(background='yellow')
+
+root.mainloop()
